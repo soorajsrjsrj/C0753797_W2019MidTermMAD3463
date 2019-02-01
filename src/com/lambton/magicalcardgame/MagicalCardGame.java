b9a51051921993e73615d260f454e0a4b91c35cd
Sooraj S <35878516+soorajsrjsrj@users.noreply.github.com>
2019-01-31 16:51:49 -0800

    Added cardlist

--- src/com/lambton/magicalcardgame/MagicalCardGame.java
+++ src/com/lambton/magicalcardgame/MagicalCardGame.java
@@ -18,7 +18,17 @@ public class MagicalCardGame {
                 {"K","7","2"},
                 {"5","9","8"} };
         gs.setCardList(w);
+
+
+                System.out.println(w[0][0]+" "+w[1][0]+" "+w[2][0]+" ");
+        System.out.println(w[0][1]+" "+w[1][1]+" "+w[2][1]+" ");
+
+        System.out.println(w[0][2]+" "+w[1][2]+" "+w[2][2]+" ");
+
+
         System.out.println();
+
+
 
 
         String a=sc.nextLine();
